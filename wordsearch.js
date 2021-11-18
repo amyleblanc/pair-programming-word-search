b4096c250b92e39c55f10544fafb9526bb1b2802
Amy <amymccarthy2014@gmail.com>
2021-11-18 05:07:44 +0000

    create a function to search a 2D array of strings for particular words

--- wordsearch.js
+++ wordsearch.js
@@ -1,8 +1,43 @@
-const wordSearch = (letters, word) => { 
-    const horizontalJoin = letters.map(ls => ls.join(''))
-    for (l of horizontalJoin) {
-        if (l.includes(word)) return true
-    }
-}
+/** @format */
 
-module.exports = wordSearch
+const wordSearch = (letters, word) => {
+    let transposedArray = transpose(letters);
+	const horizontalJoin = letters.map((ls) => ls.join(""));
+	const verticalJoin = transposedArray.map((ls) => ls.join(""));
+
+    for (let h = 0; h < horizontalJoin.length; h++) {
+        if (horizontalJoin[h].includes(word)) {
+            return true;
+        // } else if (!horizontalJoin[h].includes(word)){
+        //     return false;
+        }
+    }
+
+    for (let v = 0; v < verticalJoin.length; v++) {
+        if (verticalJoin[v].includes(word)) {
+            return true;
+        // } else if (!verticalJoin[v].includes(word)){
+        //     return false;
+        }
+    }
+};
+
+const transpose = function (matrix) {
+	let newMatrix = [];
+	for (let i = 0; i < matrix.length; i++) {
+		newMatrix.push([]);
+	}
+	for (let i = 0; i < matrix.length; i++) {
+		for (let j = 0; j < matrix[i].length; j++) {
+			if (matrix.length === 1) {
+				newMatrix[i].push([matrix[i][j]]);
+				console.log(matrix[i][j]);
+			} else {
+				newMatrix[j].push(matrix[i][j]);
+			}
+		}
+	}
+	return newMatrix;
+};
+
+module.exports = wordSearch;
