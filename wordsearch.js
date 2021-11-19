2fad56acfa5b7d06a9cb212e4d58b7a036b215ba
Amy <amymccarthy2014@gmail.com>
2021-11-19 04:10:30 +0000

    fixed bugs to make tests pass

--- wordsearch.js
+++ wordsearch.js
@@ -1,43 +1,40 @@
 /** @format */
+const verticalArray = (letters) => {
+  let newLetters = [];
 
-const wordSearch = (letters, word) => {
-    let transposedArray = transpose(letters);
-	const horizontalJoin = letters.map((ls) => ls.join(""));
-	const verticalJoin = transposedArray.map((ls) => ls.join(""));
+  for (let i = 0; i < letters[0].length; i++) {
+    newLetters.push([]);
+  }
 
-    for (let h = 0; h < horizontalJoin.length; h++) {
-        if (horizontalJoin[h].includes(word)) {
-            return true;
-        // } else if (!horizontalJoin[h].includes(word)){
-        //     return false;
-        }
+  for (let j = 0; j < letters.length; j++) {
+    for (let k = 0; k < letters[j].length; k++) {
+      newLetters[k].push(letters[j][k]);
     }
+  }
 
-    for (let v = 0; v < verticalJoin.length; v++) {
-        if (verticalJoin[v].includes(word)) {
-            return true;
-        // } else if (!verticalJoin[v].includes(word)){
-        //     return false;
-        }
-    }
+  console.log(newLetters);
+  return newLetters;
 };
 
-const transpose = function (matrix) {
-	let newMatrix = [];
-	for (let i = 0; i < matrix.length; i++) {
-		newMatrix.push([]);
-	}
-	for (let i = 0; i < matrix.length; i++) {
-		for (let j = 0; j < matrix[i].length; j++) {
-			if (matrix.length === 1) {
-				newMatrix[i].push([matrix[i][j]]);
-				console.log(matrix[i][j]);
-			} else {
-				newMatrix[j].push(matrix[i][j]);
-			}
-		}
-	}
-	return newMatrix;
+const wordSearch = (letters, word) => {
+  if (letters.length === 0) {
+    return undefined;
+  }
+  const horizontalJoin = letters.map((ls) => ls.join(""));
+  const verticalJoin = verticalArray(letters).map((ls) => ls.join(""));
+  for (let array of horizontalJoin) {
+    if (array.includes(word)) {
+      return true;
+    }
+  }
+  for (let array of verticalJoin) {
+    if (array.includes(word)) {
+      return true;
+    }
+  }
+  return false;
 };
 
 module.exports = wordSearch;
+
+//pair programming exercise Paola & Amy Nov 17 - 18, 2021
